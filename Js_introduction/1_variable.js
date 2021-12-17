98cf6c1af045021ad29f109a5137152870ebad3b
iamayushdas <ayush24das@gmail.com>
2021-12-17 22:22:47 +0530

    1_variables

---
+++ Js_introduction/1_variable.js
@@ -0,0 +1,39 @@
+// variable Declaration
+
+// var, let, const
+
+// JS is dyanamically typed language
+
+// Problems with var keyword
+
+var a = 1;
+console.log(a);
+
+var a = 'A string';
+console.log(a);
+
+var a = true;
+console.log(a);
+
+// Datatypes in JS
+//     number
+//     String
+//     Boolean
+//     undefined
+//     null
+
+let b = 1;
+// let a = 2;   //error using the same variable twice
+b = 2;           // reassigning is possible
+console.log(b);
+
+const c = 'Ayush';
+// const c = 'Shivani'; //error using the same variable twice
+// c = 'Shivani';       // reassigning not possible
+console.log(c);
+
+
+
+
+
+
