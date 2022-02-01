e1d14d95b1ea5ae0133e5e0a2d1366e1c41b44f7
iamayushdas <ayush24das@gmail.com>
2022-02-01 23:06:24 +0530

    split and join function done

---
+++ Js_introduction/Higher_Order_Function/splitJoin.js
@@ -0,0 +1,20 @@
+let str = 'pepcoder'
+
+
+// split
+
+let partOfString = str.split('p')
+// split function will split a string according to a passed parameter
+// and will return the left and the right part in an array
+console.log(partOfString);
+
+
+
+
+// join
+
+let joinStr  = partOfString.join('p')
+// join func will take an  array and it will fill the empty spaces
+// with the passed parametet and returns a complete string
+
+console.log(joinStr);
