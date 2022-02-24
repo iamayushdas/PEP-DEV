ea21b88a713c8f89d75892551727340b800d5592
iamayushdas <ayush24das@gmail.com>
2022-02-25 00:10:54 +0530

    hk: answers added for question

--- Projects/Automation/demo_project/hk.js
+++ Projects/Automation/demo_project/hk.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer')
 
 let email = "ripijo1424@ishop2k.com"
 let password = "fakepassword"
-
+const codeFile = require('./code')
 let page
 
 let browserWillBeLaunchedPromise = puppeteer.launch({
@@ -33,8 +33,23 @@ browserWillBeLaunchedPromise.then(function (browserInstance) {
 
     let algoWillBeClickedPromise = waitAndClick('.topic-card a[data-attr1="algorithms"]', page)
     return algoWillBeClickedPromise
-}).then(function () {
-    console.log('algo clicked');
+}).then(function(){
+    console.log('algoritm section clicked');
+})
+.then(function(){
+    let warmUpSectionClickedPromise = waitAndClick('input[value="warmup"]', page)
+    return warmUpSectionClickedPromise
+}).then(function(){
+    console.log('warmUp Section Clicked');
+})
+.then(function(){
+    let challengesArrPromise = page.$$('.challenge-submit-btn')
+    return challengesArrPromise
+}).then(function(questionsArr){
+    console.log("No of question" + questionsArr.length);
+
+    let questionWillBeSolved = questionSolver(page, questionsArr[0], codeFile.answers[0])
+    return questionWillBeSolved
 })
 
 function waitAndClick(selector, cPage) {
@@ -47,6 +62,29 @@ function waitAndClick(selector, cPage) {
             resolve()
         }).catch(function () {
             reject()
+        })
+    })
+}
+
+function questionSolver(page, question, answer){
+    return new Promise(function(resolve, reject){
+        let questionWillBeClickedPromise = question.click()
+        questionWillBeClickedPromise.then(function(){
+            console.log('question clicked');
+            let editorInFocusPromise = waitAndClick('.monaco-editor.no-user-select.vs', page)
+            return editorInFocusPromise
+        })
+        .then(function(){
+            return waitAndClick('.checkbox-input', page)
+        })
+        .then(function(){
+            return page.waitForSelector('textarea.custominput', page)
+        })
+        .then(function(){
+            return page.type('textarea.custominput', answer, {delay: 10})
+        })
+        .then(function(){
+            return page.click('.ui-btn.ui-btn-normal.ui-btn-primary.pull-right.hr-monaco-submit.ui-btn-styled', {delay: 10})
         })
     })
 }
