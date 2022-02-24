efbbee87bb55d9320658d5ae7cc98345748ecf17
iamayushdas <ayush24das@gmail.com>
2022-02-24 22:05:02 +0530

    HK automation: algorithm card clicked done

--- Projects/Automation/demo_project/hk.js
+++ Projects/Automation/demo_project/hk.js
@@ -11,21 +11,42 @@ let browserWillBeLaunchedPromise = puppeteer.launch({
     defaultViewport: null
 })
 
-browserWillBeLaunchedPromise.then(function(browserInstance){
-    let newTabPromise = browserInstance.newPage();   
+browserWillBeLaunchedPromise.then(function (browserInstance) {
+    let newTabPromise = browserInstance.newPage();
     return newTabPromise
-}).then(function(newTab){
+}).then(function (newTab) {
     page = newTab
     let pageWillBeOpenedPromise = newTab.goto('https://hackerrank.com/auth/login')
     return pageWillBeOpenedPromise
-}).then(function(webPage){
+}).then(function (webPage) {
     console.log('Page openened');
-    let typeEmailPromise = page.type("#input-1" , email, {delay : 50})
+    let typeEmailPromise = page.type("#input-1", email, { delay: 50 })
     return typeEmailPromise
 
-}).then(function(){
-    let typePasswordPromise = page.type("#input-2" , password, {delay : 50})
+}).then(function () {
+    let typePasswordPromise = page.type("#input-2", password, { delay: 50 })
     return typePasswordPromise
-}).then(function(){
-    page.click('button[data-analytics="LoginPassword"]', {delay : 50})
+}).then(function () {
+    let loginPromise = page.click('button[data-analytics="LoginPassword"]', { delay: 50 })
+    return loginPromise;
+}).then(function () {
+
+    let algoWillBeClickedPromise = waitAndClick('.topic-card a[data-attr1="algorithms"]', page)
+    return algoWillBeClickedPromise
+}).then(function () {
+    console.log('algo clicked');
 })
+
+function waitAndClick(selector, cPage) {
+    return new Promise(function (resolve, reject) {
+        let waitForModalPromise = cPage.waitForSelector(selector);
+        waitForModalPromise.then(function () {
+            let clickModalPromise = cPage.click(selector, { delay: 50 })
+            return clickModalPromise
+        }).then(function () {
+            resolve()
+        }).catch(function () {
+            reject()
+        })
+    })
+}
