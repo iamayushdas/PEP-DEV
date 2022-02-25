336771f2d02bd84f033f69bff6ba79f4b68f7fc7
iamayushdas <ayush24das@gmail.com>
2022-02-25 08:19:05 +0530

    portfilio : work section done

--- Projects/Portfolio/frontend_react/src/components/SocialMedia.jsx
+++ Projects/Portfolio/frontend_react/src/components/SocialMedia.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
-import { BsTwitter, BsInstagram } from 'react-icons/bs';
-import { FaFacebookF } from 'react-icons/fa';
+import { BsTwitter, BsInstagram, BsGithub, BsLinkedin } from 'react-icons/bs';
+import { FaFacebookF, FaLinkedin } from 'react-icons/fa';
 
 const SocialMedia = () => {
   return (
     <div className='app__social'>
         <div>
-            <BsTwitter />
+          <a href="https://github.com/iamayushdas"><BsGithub /></a>           
         </div>
         <div>
-            <BsInstagram />
+          <a href="https://www.linkedin.com/in/iamayushdas"><BsLinkedin /></a>           
         </div>
         <div>
-            <FaFacebookF />
+          <a href="https://twitter.com/imayushdas"><BsTwitter /></a>           
+        </div>
+        <div>
+        <a href="https://instagram.com/iamayushdas"><BsInstagram /></a>
+        </div>
+        <div>
+        <a href="https://facebook.com/iamayushdas"><FaFacebookF /></a>
         </div>
     </div>
   )
