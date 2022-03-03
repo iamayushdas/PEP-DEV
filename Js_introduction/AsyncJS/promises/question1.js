716412fcbf6226eb04a98ae7eb653d6fd13d5b75
iamayushdas <ayush24das@gmail.com>
2022-03-03 22:29:45 +0530

    rush commit

--- Js_introduction/AsyncJS/promises/question1.js
+++ Js_introduction/AsyncJS/promises/question1.js
@@ -1,28 +1,49 @@
-function placeOrder(drink){
-    return new Promise(function(resolve, reject){
-        if(drink == 'coffee'){
+function placeOrder(drink) {
+    return new Promise(function (resolve, reject) {
+        if (drink == 'coffee') {
             resolve('order for coffee ')
-        }else{
+        } else {
             reject('order cannot be placed')
         }
     })
 }
 
-function processOrder(order){
-    return new Promise(function(resolve){
+function processOrder(order) {
+    return new Promise(function (resolve) {
         console.log('order is being processed');
         resolve(`${order} served`)
     })
 }
 
-placeOrder('coffee').then(function(demand){
-    console.log(demand);
-    let orderIsProcessed = processOrder(demand)
-    return orderIsProcessed
-})
-.then(function(orderServed){
-    console.log(orderServed);
-})
-.catch(function(err){
-    console.log(err);
-})
+// promisified solution
+
+// placeOrder('coffee').then(function(demand){
+//     console.log(demand);
+//     let orderIsProcessed = processOrder(demand)
+//     return orderIsProcessed
+// })
+// .then(function(orderServed){
+//     console.log(orderServed);
+// })
+// .catch(function(err){
+//     console.log(err);
+// })
+
+
+// Async-await solution
+
+async function serveOrder() {
+
+    try {
+        let orderPlaced = await placeOrder('coffee');
+        console.log(orderPlaced);  // order for coffee
+
+        let processedOrder = await processOrder(orderPlaced);
+        console.log(processedOrder);
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
+serveOrder()
