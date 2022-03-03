aed8fd27f5c03203103cc466de9a0116195a1f55
iamayushdas <ayush24das@gmail.com>
2022-03-03 22:00:50 +0530

    coffee promise

---
+++ Js_introduction/AsyncJS/promises/question1.js
@@ -0,0 +1,25 @@
+function placeOrder(drink){
+    return new Promise(function(resolve, reject){
+        if(drink == 'coffee'){
+            resolve('order for coffee ')
+        }else{
+            reject('order cannot be placed')
+        }
+    })
+}
+
+function processOrder(order){
+    return new Promise(function(resolve){
+        console.log('order is being processed');
+        resolve(`${order} served`)
+    })
+}
+
+placeOrder('coffee').then(function(demand){
+    console.log(demand);
+    let orderIsProcessed = processOrder(demand)
+    return orderIsProcessed
+})
+.then(function(orderServed){
+    console.log(orderServed);
+})
