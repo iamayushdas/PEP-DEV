3a1f5a512c24c4eedeacc2370c0f6722c577ad93
iamayushdas <ayush24das@gmail.com>
2022-03-27 11:01:38 +0530

    modal done

--- Browser/project1-jira-clone/script.js
+++ Browser/project1-jira-clone/script.js
@@ -1,8 +1,21 @@
 let addBtn = document.querySelector('.add-btn')
 
+let modalCont = document.querySelector('.modal-cont')
+
+let addFlag = false;
+
 addBtn.addEventListener('click', function(e){
     // display a modal
+        // addFlag - true - modal display
+        // addFlag - false - modal not display
+    
+    addFlag = !addFlag;
 
+    if(addFlag){
+        modalCont.style.display = 'flex'
+    }else{
+        modalCont.style.display = 'none'
+    }
 
     // add a card
 
