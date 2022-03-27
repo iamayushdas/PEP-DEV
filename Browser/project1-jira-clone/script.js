68331c64b941cd932d4be17f425d796ee8c08e41
iamayushdas <ayush24das@gmail.com>
2022-03-27 21:24:07 +0530

    priority color handled

--- Browser/project1-jira-clone/script.js
+++ Browser/project1-jira-clone/script.js
@@ -4,6 +4,12 @@ let modalCont = document.querySelector('.modal-cont')
 
 let mainCont = document.querySelector('.main-cont')
 
+let colors = ['lightpink', 'lightblue', 'lightgreen', 'black']
+
+let modalPriorityColor= colors[colors.length - 1]
+
+let allPriorityColor = document.querySelectorAll('.priority-color');
+
 let addFlag = false;
 
 addBtn.addEventListener('click', function(e){
@@ -18,9 +24,18 @@ addBtn.addEventListener('click', function(e){
     }else{
         modalCont.style.display = 'none'
     }
+})
 
-    
-
+// changing Priority color
+allPriorityColor.forEach(function(colorElem){
+    colorElem.addEventListener('click', function(e){
+        //click krne k baad jitne bhi div h unse active class hata do taaki koi active na rahe 
+        allPriorityColor.forEach(function(priorityColorElem){
+            priorityColorElem.classList.remove('active')
+        })
+        // fir selected element pr active class lga do
+        colorElem.classList.add('active')
+    })
 })
 
 //Generating a ticket
@@ -30,6 +45,7 @@ modalCont.addEventListener('keydown', function(e){
     if(key == 'Shift'){
         createTicket() //this function will generate the ticket
         modalCont.style.display = 'none';
+        addFlag = false;
     }
 })
 
