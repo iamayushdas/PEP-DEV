d4be5fecb4eea96d5853b51d801b8aff451be272
iamayushdas <ayush24das@gmail.com>
2022-03-27 15:04:43 +0530

    ticket added via creating divs

--- Browser/project1-jira-clone/script.js
+++ Browser/project1-jira-clone/script.js
@@ -2,6 +2,8 @@ let addBtn = document.querySelector('.add-btn')
 
 let modalCont = document.querySelector('.modal-cont')
 
+let mainCont = document.querySelector('.main-cont')
+
 let addFlag = false;
 
 addBtn.addEventListener('click', function(e){
@@ -17,6 +19,27 @@ addBtn.addEventListener('click', function(e){
         modalCont.style.display = 'none'
     }
 
-    // add a card
+    
 
 })
+
+//Generating a ticket
+modalCont.addEventListener('keydown', function(e){
+    let key = e.key;
+
+    if(key == 'Shift'){
+        createTicket() //this function will generate the ticket
+        modalCont.style.display = 'none';
+    }
+})
+
+function createTicket(){
+    let ticketCont = document.createElement('div')
+    ticketCont.setAttribute('class', 'ticket-cont')
+
+    ticketCont.innerHTML = `<div class="ticket-color"></div>
+    <div class="ticket-id">dsafewrew</div>
+    <div class="task-area">Buy Milk</div>`
+
+    mainCont.appendChild(ticketCont);
+}
