ce5a92f702e126e068894712aa38f069bd7fee1f
iamayushdas <ayush24das@gmail.com>
2022-04-19 06:59:09 +0530

    arrow

--- React/preRequisites/arrow/arrow.js
+++ React/preRequisites/arrow/arrow.js
@@ -1,20 +1,38 @@
 // write a fn to add two numbers
 
-let add = (a,b) => {
-    return a + b;
+// let add = (a,b) => {
+//     return a + b;
+// }
+
+// console.log(add(3,4));
+
+
+// let regularFunction = function(name){
+//     console.log(`This is a ${name} function`);
+// }
+
+// let arrowFunction = (name) => {
+//     console.log(`This is an ${name} function`);
+// }
+
+
+// regularFunction('regular');
+// arrowFunction('arrow');
+
+
+let person = {
+    name : 'Shivani',
+    age : 22,
+
+    showDetails : function(){
+        console.log(this.name + ' ' + this.age); // shivani 22
+    },
+
+    showDetailsArrow : () => {
+        console.log(this.name + ' ' + this.age);  // undefined undefined
+    }
 }
 
-console.log(add(3,4));
 
-
-let regularFunction = function(name){
-    console.log(`This is a ${name} function`);
-}
-
-let arrowFunction = (name) => {
-    console.log(`This is an ${name} function`);
-}
-
-
-regularFunction('regular');
-arrowFunction('arrow');
+person.showDetails()
+person.showDetailsArrow()
